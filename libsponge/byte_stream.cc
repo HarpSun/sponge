a7d8af971b1f7394896c3170481eedc95a5ed59b
Dilras <dilras0@gmail.com>
2024-01-18 01:06:20 +0800

    update comment

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,36 +12,34 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-// ByteStream 就是一块固定大小的内存 可以对它进行读写
-// 所以初始化就是申请一块内存 大小为参数 capacity 所指定
+// ByteStream 可以看作是一个存储 byte 的双端队列
+// 一边是 writer, 另一边是 reader
+// writer 写数据是分多次写的，reader 读也是分多次读
+// 对于 reader 来说，他关注的是是否读完所有数据了，这个通过调用 eof 方法可以知道
+// 而 writer 则负责在写完数据之后调用 end_input 来告知所有数据已写完
 
-// 这里官方设置的编译器会要求你按照特定方式来写构造函数 非常 nb
-// 初始化可以直接抄下面的写法
-// 经过试错 发现规则如下（感兴趣可以看，不感兴趣直接抄就好，这些知识没啥用）
-// 1. 实例属性的初始化必须按照 member initlization list 的写法(就问你 nb 不 nb)
-// 而不是正常人那样在构造函数里面初始化 至于这个 dududu 写法怎么写参考下面
-// 2. 实例属性的初始化顺序必须和头文件声明的保持一致
-
-// size 表示内存的大小 类型是 int
-// offset 用来记录 reader 读到了哪里 类型是 int
-// endInput 表示供 writer 调用 如果数据写完了
-// byteQueue 用来存储数据 类型是 deque<char>
-// 为什么用 deque
+// 构造函数
 ByteStream::ByteStream(const size_t capacity):
+    // 实例属性的初始化顺序必须和头文件声明的保持一致
+    // size 表示内存的大小 类型是 int
+    // bytesWritten，bytesRead 分别表示读了多少 byte 和 写了多少 byte
+    // inputEnded 供 writer 调用 表示如果数据写完了
+    // byteQueue 用来存储数据 类型是 deque<char>，是一个双端队列，自身没有容量大小限制，会动态分配内存，所以需要我们来限制
     size(capacity),
     bytesWritten(0),
     bytesRead(0),
     inputEnded(false),
     byteQueue() {
-
 }
 
+/*
+ 往内存里面写数据 但是因为我们 bytestream 大小有限，所以要考虑 data 超过内存大小的情况
+ 这里采用简单的方案
+ 1. 检查 data 长度
+ 2. 如果超了就把超过的部分丢掉
+ 3. 写入
+ */
 size_t ByteStream::write(const string &data) {
-    // 往内存里面写数据 但是因为我们内存大小有限 所以要考虑 data 超过内存大小的情况
-    // 这里采用简单的方案
-    // 1. 检查 data 长度
-    // 2. 如果超了就把超过的部分丢掉
-    // 3. 写入内存
     string writtenData = data;
     size_t emptySize = this->size - this->byteQueue.size();
     if (data.size() > emptySize) {
@@ -55,30 +53,13 @@ size_t ByteStream::write(const string &data) {
     return writtenData.size();
 }
 
-//! \param[in] len bytes will be copied from the output side of the buffer
-string ByteStream::peek_output(const size_t len) const {
-    // 有一个边界情况就是如果 len 超过了数据的长度怎么处理
-    // 这里先不管
-    string res = "";
-    for (size_t i = 0; i < len; i++) {
-        res += this->byteQueue[i];
-    }
-    return res;
-}
-
-//! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) {
-    for (size_t i = 0; i < len; i++) {
-        this->byteQueue.pop_front();
-    }
-    this->bytesRead += len;
-}
-
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
+/*
+ 读取 len 长度的 byte 并返回
+ */
 std::string ByteStream::read(const size_t len) {
-    // 和 pop_output 很相似 唯一的区别是要返回读到的数据
     string res = "";
     for (size_t i = 0; i < len; i++) {
         // c++ dequeu pop_front 不返回 pop 掉的元素。。。
@@ -90,36 +71,83 @@ std::string ByteStream::read(const size_t len) {
     return res;
 }
 
+//! \param[in] len bytes will be copied from the output side of the buffer
+/*
+ 查看 len 长度的 byte 数据，但是不读取出来
+ */
+string ByteStream::peek_output(const size_t len) const {
+    // 有一个边界情况就是如果 len 超过了数据的长度怎么处理
+    // 这里先不管
+    string res = "";
+    for (size_t i = 0; i < len; i++) {
+        res += this->byteQueue[i];
+    }
+    return res;
+}
+
+//! \param[in] len bytes will be removed from the output side of the buffer
+/*
+ 丢弃 len 长度的 byte 数据
+ */
+void ByteStream::pop_output(const size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        this->byteQueue.pop_front();
+    }
+    this->bytesRead += len;
+}
+
+/*
+ 由 writer 负责调用，告知已完成写入
+ */
 void ByteStream::end_input() {
     this->inputEnded = true;
 }
 
+/*
+ 获取 inputEnded 属性的值
+ */
 bool ByteStream::input_ended() const {
     return this->inputEnded;
 }
 
+/*
+ 获取 bytestream 里面由多少 byte 的数据
+ */
 size_t ByteStream::buffer_size() const {
-    // buffer 就是还没有读的数据
     return this->byteQueue.size();
 }
 
+/*
+ bytestream 是否为空，要注意为空并不一定是已经读完了，还有可能是新数据还没写入
+ */
 bool ByteStream::buffer_empty() const {
     return this->byteQueue.size() == 0;
 }
 
+/*
+ 是否已经读完，满足 bytestream 为空，并且 inputEnded 被设置为 true
+ */
 bool ByteStream::eof() const {
-    // 数据是否读完 和 buffer_empty 有啥区别？？？
     return this->byteQueue.size() == 0 and this->inputEnded;
 }
 
+/*
+ 写了多少 byte 数据
+ */
 size_t ByteStream::bytes_written() const {
      return this->bytesWritten;
 }
 
+/*
+ 读了多少 byte 数据
+ */
 size_t ByteStream::bytes_read() const {
     return this->bytesRead;
 }
 
+/*
+ bytestream 还剩下多少空间可供写入
+ */
 size_t ByteStream::remaining_capacity() const {
     return this->size - this->byteQueue.size();
 }
