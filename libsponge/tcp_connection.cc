3b1f27b0458f917f23b937a18490f2bcc79056ba
Dilras <dilras0@gmail.com>
2024-03-24 23:28:45 +0800

    lab4 update

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,21 +12,26 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const {
+    return _sender.stream_in().remaining_capacity();
+}
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
 size_t TCPConnection::time_since_last_segment_received() const { return {}; }
 
 void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
 
-bool TCPConnection::active() const { return {}; }
+bool TCPConnection::active() const {
+    return not _sender.stream_in().eof() or
+           not _receiver.stream_out().eof() or
+           _linger_after_streams_finish;
+}
 
 size_t TCPConnection::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    return _sender.stream_in().write(data);
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
@@ -34,7 +39,11 @@ void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_
 
 void TCPConnection::end_input_stream() {}
 
-void TCPConnection::connect() {}
+void TCPConnection::connect() {
+    _sender.fill_window();
+    _segments_out.push(_sender.segments_out().front());
+    _sender.segments_out().pop();
+}
 
 TCPConnection::~TCPConnection() {
     try {
